879b7a4d6977b921628c5fbd3546c18a2f3dd3b3
Ru Yi Shen <ruyishen2012@gmail.com>
2022-01-28 16:05:36 -0300

    javascript base commit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function nomeDaMinhaFuncao() {
-    
-  }
+function select(element){
+    const auxi = document.querySelectorAll(".frame")
+    for(let i = 0; i<auxi.length; i++){
+        auxi[i].classList.add("hide");
+    }
+    const aux = element.querySelector(".frame");
+    aux.classList.remove("hide");
+}
+
+
