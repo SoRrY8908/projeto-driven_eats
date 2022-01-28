c3d91dcc830bf1b8cee44c50dadf3241bbceaac1
Ru Yi Shen <ruyishen2012@gmail.com>
2022-01-28 17:11:08 -0300

    javascript implement items selector commit

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-function select(element){
-    const auxi = document.querySelectorAll(".frame")
-    for(let i = 0; i<auxi.length; i++){
-        auxi[i].classList.add("hide");
+function select_food(element){
+    const food_items = document.querySelectorAll(".food .frame")
+    for(let i = 0; i<food_items.length; i++){
+        food_items[i].classList.add("hide");
+    }
+    const aux = element.querySelector(".frame");
+    aux.classList.remove("hide");
+}
+
+function select_beverage(element){
+    const beverage_items = document.querySelectorAll(".beverage .frame")
+    for(let i = 0; i<beverage_items.length; i++){
+        beverage_items[i].classList.add("hide");
+    }
+    const aux = element.querySelector(".frame");
+    aux.classList.remove("hide");
+}
+
+function select_dessert(element){
+    const dessert_items = document.querySelectorAll(".dessert .frame")
+    for(let i = 0; i<dessert_items.length; i++){
+        dessert_items[i].classList.add("hide");
     }
     const aux = element.querySelector(".frame");
     aux.classList.remove("hide");
