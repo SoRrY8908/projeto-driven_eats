e6d73e17c981327d644b211ea9577e2aedb8e4d7
Ru Yi Shen <ruyishen2012@gmail.com>
2022-01-30 12:15:23 -0300

    html css checkout commit

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-function select_food(element){
+let food_selected = false;
+let beverage_selected = false;
+let dessert_selected = false;
+
+let food_name = "";
+let beverage_name = "";
+let dessert_name = "";
+
+let food_price = "";
+let beverage_price = "";
+let dessert_price = "";
+
+
+function select_food(element, foodName, foodPrice){
     const food_items = document.querySelectorAll(".food .frame")
     for(let i = 0; i<food_items.length; i++){
         food_items[i].classList.add("hide");
     }
     const aux = element.querySelector(".frame");
     aux.classList.remove("hide");
+    food_selected = true;
+    if(food_selected && beverage_selected && dessert_selected){
+        const foot = document.querySelectorAll("footer button");
+        foot[0].classList.add("hide");
+        foot[1].classList.remove("hide");
+    }
 }
 
 function select_beverage(element){
@@ -14,6 +33,12 @@ function select_beverage(element){
     }
     const aux = element.querySelector(".frame");
     aux.classList.remove("hide");
+    beverage_selected = true;
+    if(food_selected && beverage_selected && dessert_selected){
+        const foot = document.querySelectorAll("footer button");
+        foot[0].classList.add("hide");
+        foot[1].classList.remove("hide");
+    }
 }
 
 function select_dessert(element){
@@ -23,6 +48,17 @@ function select_dessert(element){
     }
     const aux = element.querySelector(".frame");
     aux.classList.remove("hide");
+    dessert_selected = true;
+    if(food_selected && beverage_selected && dessert_selected){
+        const foot = document.querySelectorAll("footer button");
+        foot[0].classList.add("hide");
+        foot[1].classList.remove("hide");
+    }
 }
+
+function confirm_items(){
+    document.querySelector(".checkout").classList.remove("hide");
+}
+
 
 
